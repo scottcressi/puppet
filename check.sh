70f883681d87e137de5a9861d00c0e9704449def
scottcressi <scottcressi@gmail.com>
2019-05-07 17:31:35 -0400

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,3 +1,19 @@
-r10k puppetfile check
-r10k puppetfile install --color --verbose info --force
-puppet module list --modulepath modules_forge --tree
+REPO=$1
+
+[ -z "$REPO" ] && echo "please set path to puppet repo" && exit 1;
+
+if [ -x "$(command -v docker)" ]; then
+  echo "Docker is installed"
+else
+  echo "Please install Docker"
+fi
+
+if [ `sudo systemctl is-active docker` == "active" ]; then
+  echo "Docker is Running"
+else
+  echo "Please start Docker"
+fi
+
+docker run -w /var/tmp -v $REPO:/var/tmp puppet/r10k puppetfile check
+docker run -w /var/tmp -v $REPO:/var/tmp puppet/r10k puppetfile install --color --verbose info --force
+docker run -v $REPO:/var/tmp puppet/puppet-agent module list --modulepath /var/tmp/modules_forge --tree
