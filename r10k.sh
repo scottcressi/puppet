88e374af576d65570423707900d012bf5fc5d4c6
Scott Cressi <scottcressi@gmail.com>
2019-05-10 08:12:12 -0400

    path fix, r10k fix

---
+++ r10k.sh
@@ -0,0 +1,1 @@
+docker run -w /var/tmp -v ~/repos/personal/puppet/:/var/tmp -v ~/.ssh/:/root/.ssh puppet/r10k deploy environment `git rev-parse --abbrev-ref HEAD` -c r10k.yaml --verbose
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-find /etc/puppet/puppet/ssl/ -type f | grep -i `hostname` | xargs sudo rm -f
+find ssl/ -type f | grep -i `hostname` | xargs sudo rm -f
 docker run -e FACTER_virtual=kvm --net host puppet/puppet-agent agent --server puppet --verbose --no-daemonize --summarize --noop \
 --environment=production
