de70d24ea58142bb7ae74f9e9f06a2c64e6c94f0
Scott Cressi <scottcressi@gmail.com>
2019-05-11 16:41:25 -0400

    working test with systemd

--- test.sh
+++ test.sh
@@ -5,6 +5,13 @@ find docker/volumes/puppet/ssl/ca/signed -type f | grep -i `hostname` | xargs su
 docker run \
 -e FACTER_virtual=kvm \
 --net host \
-centos/systemd \
-/bin/bash \
--c "yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm && yum install -y puppet-agent-6.4.2 && /opt/puppetlabs/bin/puppet agent --verbose --no-daemonize --summarize --environment=production --server puppet"
+--name=c7 \
+--privileged \
+-d \
+-v /usr/bin/docker:/usr/bin/docker \
+-v /var/run/docker.sock:/run/docker.sock \
+diamanti/c7-systemd-dbus:latest
+
+docker exec -ti c7 /bin/bash -c "yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm"
+docker exec -ti c7 /bin/bash -c "yum install -y puppet-agent-6.4.2"
+docker exec -ti c7 /bin/bash -c "/opt/puppetlabs/bin/puppet agent --verbose --no-daemonize --summarize --environment=production --server puppet"
