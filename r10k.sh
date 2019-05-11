3d3eeb575094dcbbaa36552ffdf735a8c04797e3
Scott Cressi <scottcressi@gmail.com>
2019-05-11 12:21:27 -0400

    updates

--- r10k.sh
+++ r10k.sh
@@ -1,1 +1,11 @@
-docker run -w /var/tmp -v ~/repos/personal/puppet/:/var/tmp -v ~/.ssh/:/root/.ssh puppet/r10k deploy environment `git rev-parse --abbrev-ref HEAD` -c r10k.yaml --verbose
+# r10k deploy
+docker run \
+-w /var/tmp \
+-v ~/repos/personal/puppet/:/var/tmp \
+-v ~/.ssh/:/root/.ssh \
+puppet/r10k \
+deploy \
+environment \
+`git rev-parse --abbrev-ref HEAD` \
+-c r10k.yaml \
+--verbose
--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
-find ssl/ -type f | grep -i `hostname` | xargs sudo rm -f
-docker run -e FACTER_virtual=kvm --net host puppet/puppet-agent agent --server puppet --verbose --no-daemonize --summarize --noop \
+# clean ssl
+find docker/volumes/puppet/ssl/ca/signed -type f | grep -i `hostname` | xargs sudo rm -f
+
+# run test
+docker run \
+-e FACTER_virtual=kvm \
+--net host \
+puppet/puppet-agent:6.4.2 \
+agent \
+--server puppet \
+--verbose \
+--no-daemonize \
+--summarize \
+--noop \
 --environment=production
