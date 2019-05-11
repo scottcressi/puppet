81d5241c1b63426c20cd664a958c33bf5330ba1d
Scott Cressi <scottcressi@gmail.com>
2019-05-11 17:23:44 -0400

    fresh runs

--- test.sh
+++ test.sh
@@ -1,16 +1,18 @@
 # clean ssl
 find docker/volumes/puppet/ssl/ca/signed -type f | grep -i `hostname` | xargs sudo rm -f
 
+NAME=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
+
 # run test
 docker run \
 --net host \
---name=c7 \
+--name=$NAME \
 --privileged \
 -d \
 -v /usr/bin/docker:/usr/bin/docker \
 -v /var/run/docker.sock:/run/docker.sock \
 diamanti/c7-systemd-dbus:latest
 
-docker exec -ti c7 /bin/bash -c "yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm"
-docker exec -ti c7 /bin/bash -c "yum install -y puppet-agent-6.4.2"
-docker exec -e FACTER_virtual=kvm -ti c7 /bin/bash -c "/opt/puppetlabs/bin/puppet agent --verbose --no-daemonize --summarize --environment=production --server puppet"
+docker exec -ti $NAME /bin/bash -c "yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm"
+docker exec -ti $NAME /bin/bash -c "yum install -y puppet-agent-6.4.2"
+docker exec -e FACTER_virtual=kvm -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --verbose --no-daemonize --summarize --environment=production --server puppet"
