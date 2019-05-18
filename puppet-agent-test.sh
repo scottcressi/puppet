834473d8ad8ce9c257cb3e793375669791544f9c
Scott Cressi <scottcressi@gmail.com>
2019-05-18 12:05:19 -0400

    loop

--- puppet-agent-test.sh
+++ puppet-agent-test.sh
@@ -1,5 +1,10 @@
+set -x
+
 # clean ssl
-find docker/volumes/puppet/ssl/ca/signed -type f | grep -i `hostname` | xargs sudo rm -f
+find docker/volumes/puppet/ssl/ca/signed -type f | grep -v puppet.internal.pem | sudo xargs rm -f
+
+LIST='kvm docker'
+for i in $LIST ; do
 
 NAME=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
 
@@ -15,10 +20,11 @@ centos/systemd
 
 docker exec -ti $NAME /bin/bash -c "yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm"
 docker exec -ti $NAME /bin/bash -c "yum install -y puppet-agent-6.4.2"
-docker exec -ti $NAME /bin/bash -c "
-echo '
+docker exec -ti $NAME /bin/bash -c "echo '
 SELINUX=enforcing
 SELINUXTYPE=targeted
 ' > /etc/selinux/config
 "
-docker exec -e FACTER_virtual=kvm -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --test --verbose --no-daemonize --summarize --environment=production --server puppet"
+docker exec -e FACTER_virtual=$i -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --test --certname $NAME --verbose --no-daemonize --summarize --environment=production --server puppet"
+
+done
