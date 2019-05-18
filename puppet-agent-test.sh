0f4fca2b9211fc523442ed94d58b5185d0f50e7c
Scott Cressi <scottcressi@gmail.com>
2019-05-18 12:32:59 -0400

    prebaked image

--- puppet-agent-test.sh
+++ puppet-agent-test.sh
@@ -9,10 +9,7 @@ for i in $LIST ; do
 NAME=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
 
 # run test
-docker run --net host --name=$NAME --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd
-docker exec -ti $NAME /bin/bash -c "yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm"
-docker exec -ti $NAME /bin/bash -c "yum install -y puppet-agent-6.4.2"
-docker exec -ti $NAME /bin/bash -c "echo -e 'SELINUX=enforcing\nSELINUXTYPE=targeted\n' > /etc/selinux/config"
+docker run --net host --name=$NAME --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
 docker exec -e FACTER_virtual=$i -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --test --certname $NAME --verbose --no-daemonize --summarize --environment=production --server puppet"
 
 done
