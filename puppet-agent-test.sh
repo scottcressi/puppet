15b438690eab236e6623b6c1411a14941966c636
Scott Cressi <scottcressi@gmail.com>
2019-05-23 21:51:15 -0400

    no debug, minimal

--- puppet-agent-test.sh
+++ puppet-agent-test.sh
@@ -6,13 +6,13 @@ fi
 # clean ssl
 find docker/volumes/puppet/ssl/ca/signed -type f | grep -v puppet.internal.pem | sudo xargs rm -f
 
-LIST='kvm docker'
+LIST='docker'
 for i in $LIST ; do
 
 NAME=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
 
 # run test
 docker run --net host --name=$NAME --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
-docker exec -e FACTER_virtual=$i -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --test --certname $NAME --verbose --no-daemonize --summarize --environment=production --server puppet"
+docker exec -e FACTER_virtual=$i -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --test --certname $NAME --no-daemonize --summarize --environment=production --server puppet"
 
 done
