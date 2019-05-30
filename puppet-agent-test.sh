ed9add06dde446f64d6052fb1c6fc0cdfabc08ad
Scott Cressi <scottcressi@gmail.com>
2019-05-30 17:37:40 -0400

    functions

--- puppet-agent-test.sh
+++ puppet-agent-test.sh
@@ -3,16 +3,25 @@ echo please build puppet image
 exit 1
 fi
 
+if [ -z $1 ] ; then echo enter test/destroy ; fi
+
+test(){
 # clean ssl
 find docker/volumes/puppet/ssl/ca/signed -type f | grep -v puppet.internal.pem | sudo xargs rm -f
 
 LIST='kvm kvm'
 for i in $LIST ; do
-
-NAME=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
-
-# run test
-docker run --net docker_default --name=$NAME --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
-docker exec -e FACTER_virtual=$i -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --test --certname $NAME --no-daemonize --summarize --environment=production --server puppet"
-
+  NAME=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
+  # run test
+  docker run --net docker_default --name=$NAME --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
+  docker exec -e FACTER_virtual=$i -ti $NAME /bin/bash -c "/opt/puppetlabs/bin/puppet agent --test --certname $NAME --no-daemonize --summarize --environment=production --server puppet"
 done
+}
+
+destroy(){
+for i in `docker ps | grep centos/systemd-puppet | awk '{print $1}'` ; do
+docker stop $i
+done
+}
+
+$@
