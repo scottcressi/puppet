240ec20d1d9759906d89b33d3ccdec7a4c15d244
Scott Cressi <scottcressi@gmail.com>
2019-07-18 12:48:03 -0400

    update agent, tighten client script

--- client/client.sh
+++ client/client.sh
@@ -1,50 +1,32 @@
+if [ -z "$1" ] ; then echo missing puppet role && exit 1 ; fi
+if [ -z "$2" ] ; then echo missing puppet env && exit 1 ; fi
+if [ -z "$3" ] ; then echo missing team && exit 1 ; fi
+if [ -z "$4" ] ; then echo missing puppet server && exit 1 ; fi
+
 PUPPET_ROLE=$1
 PUPPET_ENVIRONMENT=$2
 PUPPET_TEAM=$3
-INSTANCE_ID=`< /dev/urandom tr -dc a-z | head -c${1:-16};echo;`
-echo $CERT
+PUPPET_HOST=$4
 
-prerequisites() {
- yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm
- yum install -y puppet-agent-6.4.2
-}
+# pre set
+INSTANCE_ID=$ROLE-`< /dev/urandom tr -dc a-z | head -c${1:-16};echo;`
 
-facts () {
-  puppet_role=$1
-  puppet_env=$2
-  puppet_host=$3
-  puppet_team=$4
-  vpcid=$5
+yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm
+yum install -y puppet-agent-6.6.0
 
-  echo "
-puppetserver=$puppet_host
-environment=$puppet_env
-role=$puppet_role
-team=$puppet_team
-vpcid=$vpcid
-  " > /opt/puppetlabs/facter/facts.d/facts.txt
-}
+echo "
+puppetserver=$PUPPET_HOST
+environment=$PUPPET_ENVIRONMENT
+role=$PUPPET_ROLE
+team=$PUPPET_TEAM
+" > /opt/puppetlabs/facter/facts.d/facts.txt
 
-puppetConf () {
-  puppet_env=$1
-  puppet_host=$2
-  echo "
+echo "
 [agent]
-server = $puppet_host
-environment = $puppet_env
+server      = $PUPPET_HOST
+environment = $PUPPET_ENVIRONMENT
 certname    = $INSTANCE_ID
+runinterval = 1d
 " > /etc/puppetlabs/puppet/puppet.conf
-}
 
-prerequisites
-if [ "`/opt/puppetlabs/bin/facter dmi.product.name`" == "HVM domU" ] ; then
-    MAC=`/opt/puppetlabs/bin/facter ec2_metadata.mac`
-    VPC=`/opt/puppetlabs/bin/facter ec2_metadata.network.interfaces.macs.$MAC.vpc-id`
-    PUPPET_HOST="puppet-$VPC.example.com"
-else
-    PUPPET_HOST=puppet-$PUPPET_ENVIRONMENT.example.com
-fi
-facts $PUPPET_ROLE $PUPPET_ENVIRONMENT $PUPPET_HOST $PUPPET_TEAM $VPC
-puppetConf $PUPPET_ENVIRONMENT $PUPPET_HOST
-
-/opt/puppetlabs/bin/puppet agent -t --noop
+/opt/puppetlabs/bin/puppet agent -t
