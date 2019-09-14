baa7946c4b42065d0daac8c9e2cdf51afa2d6832
Scott Cressi <scottcressi@gmail.com>
2019-09-14 18:28:44 -0400

    reorder

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,28 @@
 #!/usr/local/env bash
 
-if [ -z "$1" ] ; then
-  echo hieradata repo url required
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -x which ] ; then
+  echo which is not installed
   exit 0
 fi
 
-HIERADATA=$1
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-if ! command -v docker-compose ; then
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+if [ -x docker-compose ] ; then
+  curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
 fi
 
-if ! command -v docker ; then
+if [ -x docker-ce ] ; then
   echo Docker is not installed
   exit 0
 fi
 
-if [ ! "$(sudo systemctl is-active docker)" == "active" ]; then
+if [ ! "$(systemctl is-active docker)" == "active" ]; then
   echo Please start docker
   exit 0
 fi
 
-if ! command -v git ; then
+if [ -x git ] ; then
   echo git is not installed
   exit 0
 fi
@@ -38,21 +36,31 @@ if [ ! -d "$DIR"/../pupperware ] ; then
   git clone https://github.com/puppetlabs/pupperware.git "$DIR"/../pupperware
 fi
 
-cd "$DIR"
-bash r10k.sh
+if [ -z "$1" ] ; then
+  echo hieradata repo url required
+  exit 0
+fi
 
-cd "$DIR"/../pupperware
+HIERADATA=$1
+
+cd "$DIR"/../pupperware || exit
 export DNS_ALT_NAMES=foo
 export PUPPERWARE_ANALYTICS_ENABLED=false
 docker-compose up -d
 
-cd "$DIR"
-sudo cp hiera.yaml ../pupperware/volumes/puppet/hiera.yaml
-
-cd "$DIR"/docker
+cd "$DIR"/docker || exit
 docker-compose up -d
 
+cd "$DIR" || exit
+bash r10k.sh
+
+cd "$DIR" || exit
+if [ ! -f "$(eval echo ../pupperware/volumes/puppet/hiera.yaml)" ] ; then
+  echo no hiera.yaml found
+  exit 0
+fi
+
 if [ ! -d "$DIR"/../pupperware/volumes/puppet/hieradata ] ; then
-  sudo chmod 777 "$DIR"/../pupperware/volumes/puppet
+  chmod 777 "$DIR"/../pupperware/volumes/puppet
   git clone "$HIERADATA" "$DIR"/../pupperware/volumes/puppet/hieradata
 fi
