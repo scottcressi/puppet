5e18a6c9a067a4fc474eb83b2310587ac4241c4b
Scott Cressi <scottcressi@gmail.com>
2020-08-08 11:54:29 -0400

    r10k

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 if ! command -v docker-compose ; then echo docker-compose is not installed ;  exit 0 ; fi
 if ! command -v docker ; then echo docker is not installed ;  exit 0 ; fi
 if ! command -v git ; then echo git is not installed ;  exit 0 ; fi
+if ! command -v r10k ; then echo r10k is not installed ;  exit 0 ; fi
 [[ ! "$(systemctl is-active docker)" == "active" ]] && echo Please start docker && exit 0
 [[ ! -d /var/tmp/pupperware ]] && git clone https://github.com/puppetlabs/pupperware.git /var/tmp/pupperware
 [[ ! -d /var/tmp/puppetboard ]] && git clone https://github.com/scottcressi/docker-compose-puppetboard.git /var/tmp/puppetboard
