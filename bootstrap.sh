d07596ff207c2709b090f25673c521be5f5967d1
Scott Cressi <scottcressi@gmail.com>
2020-08-08 10:40:59 -0400

    puppetboard, git test

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,17 @@
 
 REPO=$1
 
-if ! command -v kind ; then echo which is not installed ;  exit 0 ; fi
+# checks
 if ! command -v docker-compose ; then echo docker-compose is not installed ;  exit 0 ; fi
 if ! command -v docker ; then echo docker is not installed ;  exit 0 ; fi
 if ! command -v git ; then echo git is not installed ;  exit 0 ; fi
-
 [[ ! "$(systemctl is-active docker)" == "active" ]] && echo Please start docker && exit 0
-
+[[ "$(git ls-remote > /dev/null 2>&1 ; echo $?)" -ne "0" ]] && echo git does not have permission to clone && exit 0
 [[ ! -d /var/tmp/pupperware ]] && git clone https://github.com/puppetlabs/pupperware.git /var/tmp/pupperware
 [[ ! -d /var/tmp/puppet ]] && git clone "$REPO" /var/tmp/puppet
+[[ ! -d /var/tmp/puppetboard ]] && git clone https://github.com/scottcressi/docker-compose-puppetboard.git /var/tmp/puppetboard
 
+# start puppet
 export DNS_ALT_NAMES=foo
 export PUPPERWARE_ANALYTICS_ENABLED=false
 cd /var/tmp/pupperware && docker-compose up -d
