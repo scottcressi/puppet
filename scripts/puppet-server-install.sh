124923717058d6cad97d51fb1b9f56f2f910549e
Scott Cressi <scottcressi@gmail.com>
2020-08-09 10:47:20 -0400

    puppetboard in repo

--- scripts/puppet-server-install.sh
+++ scripts/puppet-server-install.sh
@@ -7,7 +7,6 @@ if ! command -v git ; then echo git is not installed ;  exit 0 ; fi
 if ! command -v r10k ; then echo r10k is not installed ;  exit 0 ; fi
 [[ ! "$(systemctl is-active docker)" == "active" ]] && echo Please start docker && exit 0
 [[ ! -d /var/tmp/pupperware ]] && git clone https://github.com/puppetlabs/pupperware.git /var/tmp/pupperware
-[[ ! -d /var/tmp/puppetboard ]] && git clone https://github.com/scottcressi/docker-compose-puppetboard.git /var/tmp/puppetboard
 
 # start puppet
 export DNS_ALT_NAMES=foo
@@ -15,4 +14,4 @@ export PUPPERWARE_ANALYTICS_ENABLED=false
 cd /var/tmp/pupperware && docker-compose up -d
 
 echo as root run: r10k deploy environment -c r10k.yaml --verbose
-echo do not forget to manually clone to accept the fingerprint
+echo remember to manually clone to accept the fingerprint
