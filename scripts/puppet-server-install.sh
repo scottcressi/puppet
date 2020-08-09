1933cd0ac00ab1aa689bee180382c3d6cf1b31a0
Scott Cressi <scottcressi@gmail.com>
2020-08-09 19:44:13 -0400

    lock version

--- scripts/puppet-server-install.sh
+++ scripts/puppet-server-install.sh
@@ -14,6 +14,7 @@ if ! command -v r10k ; then echo r10k is not installed ;  exit 0 ; fi
 export DNS_ALT_NAMES=foo
 export PUPPERWARE_ANALYTICS_ENABLED=false
 export PUPPETDB_VERSION=6.11.0
+export PUPPETSERVER_VERSION=6.12.1
 cd /var/tmp/pupperware && docker-compose up -d
 cd "$DIR"/docker && docker-compose up -d
 
