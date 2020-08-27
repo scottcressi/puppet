f5c010728c0882d0a8c7c5e367c894c8c80c2efa
Scott Cressi <scottcressi@gmail.com>
2020-08-27 12:07:36 -0400

    simpler testing

--- scripts/puppet-agent-test.sh
+++ scripts/puppet-agent-test.sh
@@ -1,35 +1,22 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-if [ "`docker images | awk '{print $1}' | grep centos/systemd-puppet`" != "centos/systemd-puppet" ] ; then
-cd docker/build
-docker build --tag centos/systemd-puppet .
-cd $DIR
-fi
-
-if [ -z $1 ] ; then echo enter test/destroy ; fi
+if [ -z "$1" ] ; then echo enter test/destroy ; fi
 
 test(){
-LIST=(CentOS)
-ROLE=(foo)
-VIRTUAL=(kvm)
-for (( i=0; i<${#LIST[@]}; i++ )) ; do
-  echo
-  echo "## Starting: ${LIST[i]} ${ROLE[i]} ${VIRTUAL[i]} ##"
-  echo
-  NAME=`cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1`
-  docker run --net pupperware_default --name=$NAME --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
-  docker exec -ti $NAME /bin/bash -c " \
-  export FACTER_operatingsystem="${LIST[i]}" ; \
-  export FACTER_role="${ROLE[i]}" ; \
-  export FACTER_virtual="${VIRTUAL[i]}" ; \
-  /opt/puppetlabs/bin/puppet agent --test --certname $NAME --no-daemonize --summarize --environment=master --server puppet"
-done
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+    cd docker/build || exit
+    docker build --tag centos/systemd-puppet .
+    cd "$DIR" || exit
+
+    NAME="test-puppet-agent-$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)"
+    docker run --net pupperware_default --name="$NAME" -d -v /home/debian/repos/personal/puppet/scripts/facts.txt:/opt/puppetlabs/facter/facts.d/facts.txt centos/systemd-puppet
+    docker exec -ti "$NAME" /bin/bash
 }
 
 destroy(){
-for i in `docker ps | grep centos/systemd-puppet | awk '{print $1}'` ; do
-docker kill $i
-done
+    set -x
+    for i in "$(docker ps | grep centos/systemd-puppet | awk '{print $1}')" ; do
+    docker kill $i
+    done
 }
 
-$@
+"$@"
