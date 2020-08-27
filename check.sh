87d18f689920311df0f53b21fb0f758c95a76287
Scott Cressi <scottcressi@gmail.com>
2020-08-27 13:10:28 -0400

    binary checks, verbose

--- check.sh
+++ check.sh
@@ -1,5 +1,8 @@
+if ! command -v r10k ; then echo r10k gem is not installed ;  exit 0 ; fi
+if ! command -v puppet ; then echo puppet gem is not installed ;  exit 0 ; fi
+
 #r10k deploy environment --verbose
 yamllint -s -d "{extends: default, rules: {line-length: {max: 999}}}" data
 r10k puppetfile check
-r10k puppetfile install --color --verbose notice --force --moduledir modules_forge
+r10k puppetfile install --color --verbose info --force --moduledir modules_forge
 puppet module list --modulepath modules_forge --tree
