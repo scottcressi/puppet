9128480831b987f5eeb949beb6ce1018d87832d4
Scott Cressi <scottcressi@gmail.com>
2020-08-29 08:49:11 -0400

    doc, yamllint

--- README.md
+++ README.md
@@ -8,12 +8,22 @@ bash scripts/puppet-server-install.sh
 bash r10k.sh
 ```
 
-# testing
-1. edit scripts/facts.txt (optional)
+# testing (optional)
+1. edit scripts/facts.txt
 ```
 vi scripts/facts.txt
 ```
-2. run puppet agent
+2. run puppet agent container
 ```
 bash scripts/puppet-agent-test.sh test
+```
+3. run puppet agent
+```
+puppet agent -t --environment $ENV
+```
+
+# syntax checking (optional)
+1. run check script
+```
+bash check.sh
 ```
--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 if ! command -v r10k ; then echo r10k gem is not installed ;  exit 0 ; fi
 if ! command -v puppet ; then echo puppet gem is not installed ;  exit 0 ; fi
+if ! command -v yamllint ; then echo yamllimt is not installed ;  exit 0 ; fi
 
-#r10k deploy environment --verbose
 yamllint -s -d "{extends: default, rules: {line-length: {max: 999}}}" data
 r10k puppetfile check
 r10k puppetfile install --color --verbose info --force --moduledir modules_forge
