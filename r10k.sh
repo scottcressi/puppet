0f1b252f3f8fac08dafb792930d3df5e728c54f3
Scott Cressi <scottcressi@gmail.com>
2020-09-01 15:53:04 -0400

    cachedir in command

--- r10k.sh
+++ r10k.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo r10k deploy environment -c r10k.yaml --puppetfile --verbose
+sudo r10k deploy environment -c r10k.yaml --puppetfile --verbose --cachedir /var/tmp/r10k_cache
