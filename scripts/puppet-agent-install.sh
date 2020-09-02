97bd26674d9c581c38ed2a00b9540683c58e44d5
Scott Cressi <scottcressi@gmail.com>
2020-09-01 21:19:11 -0400

    cleanup

--- scripts/puppet-agent-install.sh
+++ scripts/puppet-agent-install.sh
@@ -2,33 +2,26 @@
 
 if [ -z "$1" ] ; then echo missing puppet role && exit 1 ; fi
 if [ -z "$2" ] ; then echo missing puppet env && exit 1 ; fi
-if [ -z "$3" ] ; then echo missing team && exit 1 ; fi
-if [ -z "$4" ] ; then echo missing puppet server && exit 1 ; fi
+if [ -z "$3" ] ; then echo missing puppet server && exit 1 ; fi
 
 PUPPET_ROLE=$1
 PUPPET_ENVIRONMENT=$2
-PUPPET_TEAM=$3
-PUPPET_HOST=$4
-
-# pre set
-UUID="$(head /dev/urandom | tr -dc a-z0-9 | head -c 13 ; echo '')"
-INSTANCE_ID=$ROLE-$UUID
+PUPPET_SERVER=$3
 
 yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm
 yum install -y puppet-agent-6.17.0
 
 echo "
-puppetserver=$PUPPET_HOST
+puppetserver=$PUPPET_SERVER
 environment=$PUPPET_ENVIRONMENT
 role=$PUPPET_ROLE
-team=$PUPPET_TEAM
 " > /opt/puppetlabs/facter/facts.d/facts.txt
 
 echo "
 [agent]
-server      = $PUPPET_HOST
+server      = $PUPPET_SERVER
 environment = $PUPPET_ENVIRONMENT
-certname    = $INSTANCE_ID
+certname    = $PUPPET_ROLE-$(head /dev/urandom | tr -dc a-z0-9 | head -c 13 ; echo '')
 runinterval = 30m
 " > /etc/puppetlabs/puppet/puppet.conf
 
