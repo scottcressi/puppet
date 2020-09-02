6aa0e1ec0a4f2f2666de2b43be011c6b5b98099f
Scott Cressi <scottcressi@gmail.com>
2020-09-01 21:49:32 -0400

    cleanup

--- scripts/puppet-agent-test.sh
+++ scripts/puppet-agent-test.sh
@@ -4,13 +4,10 @@ if [ -z "$1" ] ; then echo enter test/destroy ; fi
 
 test(){
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-    cd "$DIR"/docker/build || exit
-    docker build --tag centos/systemd-puppet .
-    cd "$DIR" || exit
-
     UUID="$(head /dev/urandom | tr -dc a-z0-9 | head -c 13 ; echo '')"
     NAME=test-puppet-agent-$UUID
+
+    cd "$DIR"/docker/build && docker build --tag centos/systemd-puppet .
     docker run --net pupperware_default --name="$NAME" -d -v "$DIR"/facts.txt:/opt/puppetlabs/facter/facts.d/facts.txt --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
     docker exec -ti "$NAME" /bin/bash
 }
