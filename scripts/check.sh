653e66afa900efe6013886a41c2951653a303478
Scott Cressi <scottcressi@gmail.com>
2020-09-06 11:14:05 -0400

    shellcheck

--- scripts/check.sh
+++ scripts/check.sh
@@ -6,7 +6,7 @@ if ! command -v r10k ; then echo r10k gem is not installed ;  exit 0 ; fi
 if ! command -v puppet ; then echo puppet gem is not installed ;  exit 0 ; fi
 if ! command -v yamllint ; then echo yamllimt is not installed ;  exit 0 ; fi
 
-cd $DIR/../ && yamllint -s -d "{extends: default, rules: {line-length: {max: 999}}}" data
-cd $DIR/../ && r10k puppetfile check
-cd $DIR/../ && r10k puppetfile install --color --verbose info --force --moduledir modules_forge
-cd $DIR/../ && puppet module list --modulepath modules_forge --tree
+cd "$DIR"/../ && yamllint -s -d "{extends: default, rules: {line-length: {max: 999}}}" data
+cd "$DIR"/../ && r10k puppetfile check
+cd "$DIR"/../ && r10k puppetfile install --color --verbose info --force --moduledir modules_forge
+cd "$DIR"/../ && puppet module list --modulepath modules_forge --tree
