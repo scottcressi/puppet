8ee5ee8ccea2292049d61e34c02952619a1a6276
Scott Cressi <scottcressi@gmail.com>
2020-09-07 10:32:12 -0400

    os detection, output

--- scripts/puppet-agent-install.sh
+++ scripts/puppet-agent-install.sh
@@ -8,8 +8,18 @@ if [ -z "$PUPPET_ROLE" ] ; then echo missing puppet role && exit 1 ; fi
 if [ -z "$PUPPET_ENVIRONMENT" ] ; then echo missing puppet env && exit 1 ; fi
 if [ -z "$PUPPET_SERVER" ] ; then echo missing puppet server && exit 1 ; fi
 
-yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm
-yum install -y puppet-agent-6.18.0
+echo
+echo "role:   $PUPPET_ROLE"
+echo "env:    $PUPPET_ENVIRONMENT"
+echo "server: $PUPPET_SERVER"
+echo
+
+if [ "$(awk /^ID/ /etc/os-release)" == "ID=debian" ] ; then
+    echo put debian packages here
+elif [ "$(awk /^ID/ /etc/os-release)" == "ID=centos" ] ; then
+    yum install -y https://yum.puppet.com/puppet6/puppet-release-el-7.noarch.rpm
+    yum install -y puppet-agent-6.18.0
+fi
 
 echo "
 puppetserver=$PUPPET_SERVER
