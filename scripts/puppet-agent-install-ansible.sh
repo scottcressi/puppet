5deee99974d615db28d73abc8cb296a69ff7ea2e
Scott Cressi <scottcressi@gmail.com>
2020-10-31 15:37:35 -0400

    extra args

--- scripts/puppet-agent-install-ansible.sh
+++ scripts/puppet-agent-install-ansible.sh
@@ -1,6 +1,9 @@
 if ! command -v ansible ; then echo ansible is not installed ;  exit 0 ; fi
 
+export ANSIBLE_HOST_KEY_CHECKING=False
+
 ansible-playbook \
+    --become \
     --user osboxes \
     --become-method sudo \
     --become-user root \
