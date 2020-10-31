5bdce18b587c7e7fcf7ef67c11196b93302c1d1a
Scott Cressi <scottcressi@gmail.com>
2020-10-31 15:07:27 -0400

    in command

--- scripts/puppet-agent-install-ansible.sh
+++ scripts/puppet-agent-install-ansible.sh
@@ -1,3 +1,8 @@
 if ! command -v ansible ; then echo ansible is not installed ;  exit 0 ; fi
 
-ansible-playbook -i ./puppet-agent-install-ansible-hosts.txt ./puppet-agent-install-ansible-playbook.yaml
+ansible-playbook \
+    --user osboxes \
+    --become-method sudo \
+    --become-user root \
+    -i ./puppet-agent-install-ansible-hosts.txt \
+    ./puppet-agent-install-ansible-playbook.yaml
