87079edd58c7cc0e98bc5edd1bfdc46da1360aad
Scott Cressi <scottcressi@gmail.com>
2020-10-31 12:11:49 -0400

    combine, case

--- scripts/puppet-agent-test.sh
+++ scripts/puppet-agent-test.sh
@@ -1,19 +1,50 @@
 #!/usr/bin/env bash
 
-if [ -z "$1" ] ; then echo enter test/destroy ; fi
+if ! command -v docker ; then echo docker is not installed ;  exit 0 ; fi
 
-test(){
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    UUID="$(head /dev/urandom | tr -dc a-z0-9 | head -c 13 ; echo '')"
-    NAME=test-puppet-agent-$UUID
+if [[ $# -eq 0 ]] ; then
+    echo """
+options:
+--client-test
+--client-destroy
+--cert-list
+--cert-clean \$SOMECERT
+"""
+    exit 0
+fi
 
-    cd "$DIR"/docker/build && docker build --tag centos/systemd-puppet .
-    docker run --net pupperware_default --name="$NAME" -d -v "$DIR"/facts.txt:/opt/puppetlabs/facter/facts.d/facts.txt --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
-    docker exec -ti "$NAME" /bin/bash
-}
-
-destroy(){
-    docker ps | grep test-puppet-agent | awk '{print $1}' |xargs docker kill
-}
-
-"$@"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --client-test)
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+        UUID="$(head /dev/urandom | tr -dc a-z0-9 | head -c 13 ; echo '')"
+        NAME=test-puppet-agent-$UUID
+        cd "$DIR"/docker/build && docker build --tag centos/systemd-puppet .
+        docker run \
+            -d \
+            --privileged \
+            --net pupperware_default \
+            --name="$NAME" \
+            -v "$DIR"/facts.txt:/opt/puppetlabs/facter/facts.d/facts.txt \
+            -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
+        docker exec -ti "$NAME" /bin/bash
+      ;;
+    --client-destroy)
+        docker ps | awk '/test-puppet-agent/ {print $1}' | xargs docker kill
+        exit 0
+      ;;
+    --cert-list)
+        docker exec -ti pupperware_puppet_1 puppetserver ca list --all
+        exit 0
+      ;;
+    --cert-clean)
+        docker exec -ti pupperware_puppet_1 puppetserver ca clean --certname "$2"
+        exit 0
+      ;;
+    *)
+      echo Invalid argument
+      exit 1
+      ;;
+  esac
+  shift
+done
