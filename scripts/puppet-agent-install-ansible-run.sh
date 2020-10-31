f10127c4ac891f97d99295e49cae13f031ef0522
Scott Cressi <scottcressi@gmail.com>
2020-10-31 17:43:29 -0400

    host group

--- scripts/puppet-agent-install-ansible-run.sh
+++ scripts/puppet-agent-install-ansible-run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ] ; then echo enter host group && exit 1 ; fi
 if ! command -v ansible ; then echo ansible is not installed ;  exit 0 ; fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -8,4 +9,4 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook \
     -i "$DIR"/puppet-agent-install-ansible-hosts.txt \
     "$DIR"/puppet-agent-install-ansible-playbook.yaml \
-    --limit testing
+    --limit "$1"
