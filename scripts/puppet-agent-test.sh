f28bcafe0ecb1e8327c9430ff4cf81dc886566a7
Scott Cressi <scottcressi@gmail.com>
2021-03-30 12:54:26 -0400

    metal

--- scripts/puppet-agent-test.sh
+++ scripts/puppet-agent-test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-if ! command -v docker ; then echo docker is not installed ;  exit 0 ; fi
+if ! command -v docker > /dev/null ; then echo docker is not installed ;  exit 0 ; fi
 
 if [ $# -eq 0 ] ; then
     echo """
 options:
 --client-test-docker
---client-test-vagrant
+--client-test-metal
 --client-destroy
 --cert-list
 --cert-clean \$SOMECERT
@@ -18,21 +18,18 @@ client-test-docker(){
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     UUID="$(head /dev/urandom | tr -dc a-z0-9 | head -c 13 ; echo '')"
     NAME=test-puppet-agent-$UUID
-    cd "$DIR"/docker/build && docker build --tag centos/systemd-puppet .
+    cd "$DIR"/docker/build && docker build --tag puppet-test-client .
     docker run \
-        -d \
-        --privileged \
+        -ti \
         --net pupperware_default \
         --name="$NAME" \
         -v "$DIR"/facts.txt:/opt/puppetlabs/facter/facts.d/facts.txt \
-        -v /sys/fs/cgroup:/sys/fs/cgroup:ro centos/systemd-puppet
-    docker exec -ti "$NAME" /bin/bash
+        puppet-test-client
 }
 
-client-test-vagrant(){
+client-test-metal(){
     IP=$(ip route get 8.8.8.8 | head -1 | awk '{print $7}')
-    echo "$IP"
-    vagrant up --provision
+    vagrant up --provision --provider="$PROVIDER"
     vagrant ssh -c "echo $IP puppet | sudo tee -a /etc/hosts"
     vagrant ssh -c "sudo /opt/puppetlabs/bin/puppet agent -t"
 }
@@ -42,8 +39,15 @@ while [ $# -gt 0 ]; do
     --client-test-docker)
         client-test-docker
       ;;
-    --client-test-vagrant)
-        client-test-vagrant
+    --client-test-metal)
+        echo NOTE: when using libvirt provider you must add the user that is running vagrant to the libvirt group
+        echo NOTE: to remove a previous conflicting virtualbox network run: VBoxManage hostonlyif remove vboxnetX
+        echo NOTE: to install libvirt provider run: vagrant plugin install vagrant-libvirt
+        echo NOTE: to remove duplicate domain run: sudo virsh undefine $DOMAIN
+        echo
+        [ -z "$2" ] && echo virtualbox / libvirt required && exit 0
+        PROVIDER=$2
+        client-test-metal
       ;;
     --client-destroy)
         docker ps | awk '/test-puppet-agent/ {print $1}' | xargs docker kill
